869092385a761b7a1dd8ae96557ec0c4e1a7e830
Davide Gurnari <45945769+dgurnari@users.noreply.github.com>
2020-04-15 18:06:38 +0200

    Delete run_compute_contributions.sh

--- run_compute_contributions.sh
+++
@@ -1,7 +0,0 @@
-# Calculate ecc contributions for ball files.
-# requires the value of the max filtration
-for datafile in balls/*
-do
-    echo $datafile
-    python single_ball_contribution.py "$datafile" "$1"
-done
